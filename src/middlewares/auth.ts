c1aa4073256d3c5bd45e48d6c030b5e72a5b9412
Coder. Naiem <126259025+Naiem548@users.noreply.github.com>
2026-07-07 00:22:27 +0600

    perfectly added user bannes some logic

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express';
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import AppError from '../utils/AppError';
 import config from '../config';
+import { prisma } from '../lib/prisma';
 
 export const auth = (...requiredRoles: string[]) => {
   return async (req: Request, _res: Response, next: NextFunction) => {
@@ -14,7 +15,20 @@ export const auth = (...requiredRoles: string[]) => {
 
       const decoded = jwt.verify(token, config.jwt.secret) as JwtPayload;
 
-      if (requiredRoles.length && !requiredRoles.includes(decoded.role)) {
+      const user = await prisma.user.findUnique({
+        where: { id: decoded.id },
+        select: { id: true, status: true, role: true },
+      });
+
+      if (!user) {
+        throw new AppError(401, 'User no longer exists!');
+      }
+
+      if (user.status === 'BANNED') {
+        throw new AppError(403, 'This user account has been banned!');
+      }
+
+      if (requiredRoles.length && !requiredRoles.includes(user.role)) {
         throw new AppError(403, 'You have no permission to access this route!');
       }
 
